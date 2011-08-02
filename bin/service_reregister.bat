dafa6a94521564d3459c59c16379dfc0924214e0
Brian Ewing <brianewing@gmail.com>
2011-08-02 15:47:59 +0100

    initial commit of couchbase 2.0

---
+++ bin/service_reregister.bat
@@ -0,0 +1,12 @@
+set CB_BIN=%~dp0
+set CB_ROOT=%CB_BIN%..
+set CB_ERTS=%CB_ROOT%\erts-5.8.3\bin
+
+cmd /q /c "%CB_BIN%service_stop.bat"
+
+cmd /q /c "%CB_BIN%service_unregister.bat"
+
+cmd /q /c "%CB_BIN%service_register.bat"
+
+cmd /q /c "%CB_BIN%service_start.bat"
+
